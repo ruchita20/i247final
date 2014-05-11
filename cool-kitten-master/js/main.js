c199c5a21478e17e5302cc3f82f2d7e7f62eb435
Ruchita Rathi <ruchitarathi@ischool.berkeley.edu>
2014-05-10 23:46:50 -0700

    test - tubes trend

---
+++ cool-kitten-master/js/main.js
@@ -0,0 +1,11 @@
+
+
+$( document ).ready(function() {
+  console.log("percentbar");
+$("#CategoryWiseLineChart").load("./LineChart/linechart.html");
+    	$("#PercentBar").load("./PercentBarChart/PercentBarChart.html");
+  e.preventDefault();
+
+
+});
+
